6b81df32a1b3dd557a3f005ef47e828405ba3ca6
CantarinoG <guilhermecantarino1709@gmail.com>
2025-01-17 12:45:19 -0300

    refactor: Add explicit typing

--- input_controller.py
+++ input_controller.py
@@ -6,44 +6,45 @@ class InputController:
 
     def __init__(self, pattern: list = ['r', 'd', 'l', 'u']):
         pyautogui.PAUSE = 0
-        self.pattern = pattern
-        self.current_index = 0
+        self._pattern = pattern
+        self._current_index = 0
         
-    def _press_key(self, key: str):
+    def _press_key(self, key: str) -> None:
         pyautogui.keyDown(key)
         time.sleep(0.1)
         pyautogui.keyUp(key)
 
-    def press_up(self):
+    def press_up(self) -> None:
         self._press_key('up')
 
-    def press_down(self):
+    def press_down(self) -> None:
         self._press_key('down')
 
-    def press_left(self):
+    def press_left(self) -> None:
         self._press_key('left')
 
-    def press_right(self):
+    def press_right(self) -> None:
         self._press_key('right')
 
-    def press_x(self):
+    def press_x(self) -> None:
         self._press_key('x')
 
-    def press_z(self):
+    def press_z(self) -> None:
         self._press_key('z')
 
-    def focus_window(self, window_name: str):
+    def focus_window(self, window_name: str) -> bool:
         try:
             subprocess.run(['xdotool', 'search', '--name', window_name, 'windowactivate'], check=True)
+            time.sleep(0.5)
             return True
         except:
             return False
         
-    def set_pattern(self, pattern: list):
-        self.pattern = pattern
+    def set_pattern(self, pattern: list) -> None:
+        self._pattern = pattern
 
-    def press_next_direction(self):
-        direction = self.pattern[self.current_index]
+    def press_next_direction(self) -> None:
+        direction = self._pattern[self._current_index]
         
         if direction == 'r':
             self.press_right()
@@ -54,4 +55,4 @@ class InputController:
         elif direction == 'd':
             self.press_down()
             
-        self.current_index = (self.current_index + 1) % len(self.pattern)
+        self._current_index = (self._current_index + 1) % len(self._pattern)
