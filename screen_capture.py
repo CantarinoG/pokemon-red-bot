b74dc1a54751b7eee44235d4ebc3e5c0f56e313a
CantarinoG <guilhermecantarino1709@gmail.com>
2025-01-17 13:04:05 -0300

    feat: Add screen capture class

---
+++ screen_capture.py
@@ -0,0 +1,17 @@
+import os
+import pyscreenshot as ImageGrab
+import cv2
+import numpy as np
+
+class ScreenCapture:
+
+    def __init__(self):
+        self.screen_image: ImageGrab.Image = None
+
+    def capture_screen(self) -> None:
+        screen_image = ImageGrab.grab()
+        self.screen_image = screen_image
+    
+    def get_screen_section(self, bbox: tuple) -> np.ndarray:
+        section = self.screen_image.crop(bbox)
+        return cv2.cvtColor(np.array(section), cv2.COLOR_RGB2BGR)
